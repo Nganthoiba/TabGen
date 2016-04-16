7b70994ad3e15b148bae290ec9a460527c61bbde
Nganthoiba <leecba@gmail.com>
2016-04-16 19:12:42 +0530

    slide changes in getting the channels for chat

--- app/src/main/java/expandableLists/ExpandableListDataPump.java
+++ app/src/main/java/expandableLists/ExpandableListDataPump.java
@@ -38,7 +38,7 @@ public class ExpandableListDataPump {
         //channelList = OrganisationDetails.getListOfChannel(user_id);
 
         try {
-            ConnectServer channelIdList = new ConnectServer("http://"+sp.getServerIP_Preference(context)+"/TabGenAdmin/getChannelsID.php"+
+            ConnectServer channelIdList = new ConnectServer("http://"+sp.getServerIP_Preference(context)+"/TabGenAdmin/getChannels.php"+
                     "?user_id="+user_id);
             String jsonStr = channelIdList.convertInputStreamToString(channelIdList.getData());
             //saving channel details in share preference
@@ -50,8 +50,8 @@ public class ExpandableListDataPump {
                     JSONArray jsonArray1 = jsonObj1.getJSONArray("team_list");
                     JSONArray jsonArray2 = jsonObj1.getJSONArray("channels");
                     for(int i=0;i<jsonArray1.length();i++){//for every item(team) in the team list
-                        JSONObject jsonObj2 = jsonArray1.getJSONObject(i);
-                        team_name = jsonObj2.getString("team_name");//getting the team name
+                        //JSONObject jsonObj2 = jsonArray1.getJSONObject(i);
+                        team_name = jsonArray1.getString(i);//getting the team name
                         System.out.println("Team name--> " + team_name);
                         channelList = new ArrayList<String>();
 
@@ -60,16 +60,23 @@ public class ExpandableListDataPump {
                         try {
                             JSONArray jsonArray3 = jsonObj3.getJSONArray(team_name);
 
-                            if (jsonArray3.length() != 0) {
+                            if (jsonArray3.length() != 0 && jsonArray3 != null) {
                                 for (int j = 0; j < jsonArray3.length(); j++) {
                                     JSONObject jsonObj4 = jsonArray3.getJSONObject(j);
-                                    channelList.add(jsonObj4.getString("Channel_name"));
-                                    System.out.println("Channel Name: " + jsonObj4.getString("Channel_name"));
+                                    if(jsonObj4!=null) {
+                                        channelList.add(jsonObj4.getString("Channel_name"));
+                                        System.out.println("Channel Name: " + jsonObj4.getString("Channel_name"));
+                                    }
+                                    else {
+                                        channelList.add("NO CHANNEL AVAILABLE");
+                                        continue;
+                                    }
                                 }
                                 expandableListDetail.put(team_name, channelList);
                             } else channelList.add("No channel available");
                         } catch (Exception e) {
-                            System.out.println("Unable to parse channel: \n" + e.toString());
+                            System.out.println("System crashes here. Unable to parse channel: \n" + e.toString());
+                            continue;
                         }
 
                     }
